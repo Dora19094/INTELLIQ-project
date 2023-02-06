fc1295b5d61f2a0b73d221241ab6b6d69593b374
ARGYRO <el19950@mail.ntua.gr>
2023-02-07 01:45:42 +0200

    fixed bug on Answer Area and created SessionAnswersArea to show the answers the user chose (view answers).

---
+++ frontend/src/components/SessionAnswersArea.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function SessionAnswersArea() {
+// const [ans, setAns] = useState();
+//   useEffect(() => {
+//     const url = `http://localhost:3001/getsessionanswers/${questionnaireID}/${session}`;
+    
+//     const fetchData = async () => {
+//       await fetch(url)
+//         .then((response) => response.json())
+//         .then((data) => {console.log(data); const d = [data]; setAns(d)});
+//     };
+
+//     fetchData();
+// }, [] ); 
+
+// ans.opttext
+
+  return (
+    <div>
+      answers!
+      {/* <div className="questionnaires-grid">
+      {ans.map((an) => (
+          <div className="d-flex justify-content-end">
+          <h3>{an.qtext
+          an.opttext}</h3>
+        </div>
+      ))}
+    </div> */}
+    </div>
+  )
+}
