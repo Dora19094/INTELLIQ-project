e26f411f5e6164f9ba8ec9dab0f94fa14137a29f
ntua-el19104 <115741679+ntua-el19104@users.noreply.github.com>
2023-02-10 22:54:20 +0200

    https setup

---
+++ api-backend/app.js
@@ -0,0 +1,40 @@
+//UNDER DEVELOPMENT
+//DONT MIND ABOUT THIS CODE
+
+const express = require('express');
+const bodyParser = require('body-parser');
+var cors = require('cors');
+
+//setup express app
+const app = express();
+app.use(cors({
+    origin: '*'
+}));
+
+//connect to MongoDB
+const connection = require('./db');
+
+//middleware for accesing data in json
+app.use(bodyParser.json());
+
+//admin endpoints
+app.use('/admin',require('./routes/admin.js'));
+
+//intelliQ routes
+app.use(require('./routes/intelliQ.js'));
+
+//errors from bad requests
+app.all('*',(req,res,next)=>{
+    try{
+        throw new Error('Wrong URI or Wrong Method')
+    } catch(err){next(err)}
+});
+
+//middleware for error handling
+app.use((err,req,res,next)=>{
+    console.log(`error ${err.message}`);
+    const status = err.status || 400;
+    res.status(status).send({status:"Failed",error:err.message})
+});
+
+module.exports = app;
