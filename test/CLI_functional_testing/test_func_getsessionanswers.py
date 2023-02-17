a334003781fcbf9415e639a2e992a04c8a6d601f
ntua-el19606 <el19606@mail.ntua.gr>
2023-02-17 12:57:25 +0200

    Update test_func_getsessionanswers.py

--- test/CLI_functional_testing/test_func_getsessionanswers.py
+++ test/CLI_functional_testing/test_func_getsessionanswers.py
@@ -8,7 +8,7 @@ os.environ['PYTHONIOENCODING'] = 'utf-8'
 def contains_two_words(text, word1, word2):
     return word1 in text and word2 in text
 
-command = ["python", "cli.py", "getsessionanswers", "--questionnaire_id",  "63e9404266b76729edd6d133", "--session", "ekis"]
+command = ["python", "cli.py", "getsessionanswers", "--questionnaire_id",  "63e9404266b76729edd6d133", "--session", "FXeP"]
 
 # Here we are running the API call getsessionanswers with the respecitve questionnaire_id and session.We are also capturing the output.
 result = subprocess.run(command, stdout=subprocess.PIPE)
@@ -19,8 +19,8 @@ output_str = result.stdout.decode('utf-8')
 print(output_str)
 
 #We are checking if the outcome of the API call has the right questionnaire_id and session.
-id_ = '"63ea43e87447803cd60c2082"'
-session = '"ekis"'
+id_ = '"63ea43e87447803cd60c2083"'
+session = '"FXeP"'
 
 if contains_two_words(output_str, id_,session):
      print("The .exe file returned the expected JSON document.")
